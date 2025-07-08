fd0e82418ca2148c3ac906c287d70f8524ab968c
Harshitha390 <harshmahadevappa@gmail.com>
2025-07-08 15:14:13 +0530

    Add files via upload

---
+++ TypeScript.js
@@ -0,0 +1,4 @@
+var userName = "sunil";
+var age = 23;
+var isActive = true;
+console.log("Name:  ".concat(userName, ", Age:  ").concat(age, ", IsActive: ").concat(isActive));
---
+++ TypeScript.ts
@@ -0,0 +1,4 @@
+let userName:string="sunil";
+let age:number=23;
+let isActive:boolean=true;
+console.log(`Name:  ${userName}, Age:  ${age}, IsActive: ${isActive}`);
